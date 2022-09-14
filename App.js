212b16fffa92adde9859bf684c41f3ae8b1635f5
William Berbet <wiiliamb@hotmail.com>
2022-09-14 15:45:23 -0300

    app finalizado

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
 import React, { useState } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, FlatList } from 'react-native'
+import { FontAwesome } from '@expo/vector-icons'
+import Tarefa from './src/Tarefa'
 
 export default function App() {
-  const [name, setName] = useState('William')
+  const [tarefa, setTarefa] = useState('')
+  const [list, setList] = useState([])
 
-  function handleMudaNome() {
-    setName('Maria Amalia')
+  function handleAdd() {
 
+    if (tarefa === '') {
+      return;
+    }
+
+    const dados = {
+      key: Date.now(),
+      item: tarefa
+    }
+
+    setList(oldArray => [dados, ...oldArray]);
+    setTarefa('')
+  }
+
+  function handleDelete(item) {
+    let filtroItem = list.filter((tarefa) => {
+      return (tarefa.item !== item)
+    })
+
+    setList(filtroItem)
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{name}</Text>
-      <TouchableOpacity style={styles.button} onPress={handleMudaNome}>
-        <Text style={styles.buttonText}>Mudar Nome</Text>
-      </TouchableOpacity>
+      <Text style={styles.title}>Tarefas</Text>
+
+      <View style={styles.containerInput}>
+        <TextInput
+          placeholder='Digite sua tarefa..'
+          style={styles.input}
+          value={tarefa}
+          onChangeText={(text) => setTarefa(text)}
+        />
+
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
+          <FontAwesome name='plus' size={20} color='#F6BE00' />
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={list}
+        keyExtractor={(item) => item.key}
+        renderItem={({ item }) => <Tarefa data={item} deleteItem={() => handleDelete(item.item)} />}
+        style={styles.list}
+
+      />
     </View>
   )
 }
@@ -26,20 +65,43 @@ const styles = StyleSheet.create({
     paddingTop: 28,
   },
   title: {
-    fontSize: 52,
-    color: '#F6BE00',
     fontWeight: 'bold',
-    textAlign: 'center'
+    fontSize: 24,
+    color: '#F6BE00',
+    marginTop: '5%',
+    paddingStart: '5%',
+    marginBottom: 12,
   },
-  button: {
-    backgroundColor: '#F6BE00',
-    height: 40,
-    justifyContent: 'center',
+  containerInput: {
+    flexDirection: 'row',
+    width: '100%',
+    height: 44,
     alignItems: 'center',
-    marginTop: 35
+    justifyContent: 'center',
+    marginBottom: 22,
   },
-  buttonText: {
-    color: '#121212',
-    fontWeight: 'bold'
+  input: {
+    width: '75%',
+    backgroundColor: '#FBFBFB',
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
+  buttonAdd: {
+    width: '15%',
+    height: 44,
+    backgroundColor: '#73f7ff',
+    marginLeft: 6,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4,
+  },
+  list: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingStart: '4%',
+    paddingEnd: '4%',
+
+
   }
 })
