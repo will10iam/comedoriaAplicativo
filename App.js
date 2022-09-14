76db86d9636950261899b0b93cddc0e2c7533930
William Berbet <wiiliamb@hotmail.com>
2022-09-14 12:42:59 -0300

    primeira config

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default function App() {
+  const [name, setName] = useState('William')
+
+  function handleMudaNome() {
+    setName('Maria Amalia')
+
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>{name}</Text>
+      <TouchableOpacity style={styles.button} onPress={handleMudaNome}>
+        <Text style={styles.buttonText}>Mudar Nome</Text>
+      </TouchableOpacity>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#060D13',
+    paddingTop: 28,
   },
-});
+  title: {
+    fontSize: 52,
+    color: '#F6BE00',
+    fontWeight: 'bold',
+    textAlign: 'center'
+  },
+  button: {
+    backgroundColor: '#F6BE00',
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 35
+  },
+  buttonText: {
+    color: '#121212',
+    fontWeight: 'bold'
+  }
+})
